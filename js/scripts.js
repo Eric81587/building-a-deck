1547501df2b595c4d56a166b62b2652f21aefcbe
Eric Roragen-Eggers <Eric.D.Eggers@Gmail.com>
2021-03-29 14:47:11 -0700

    initial commit
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+let suitColors = ["Red", "Black"]
+let suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+let cards = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
